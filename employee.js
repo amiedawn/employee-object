0ce2e94368d4616f412ac0312697ce2d1d3b5edb
amiedawn <amie.d.carroll@gmail.com>
2021-11-19 07:23:55 -0700

    no output for sal in console.log

--- employee.js
+++ employee.js
@@ -1,24 +1,30 @@
 let emp = {
-  empId: "",
-  empName: "",
-  baseSal: "",
-  baseHrs: "",
-  extraHrs: "",
+  empId: " ",
+  empName: " ",
+  baseSal: " ",
+  baseHrs: " ",
+  extraHrs: " ",
 };
 
 let consultant = {
+  empId: "30",
+  empName: "Sheila Harrington",
   baseSal: 4000,
   __proto__: emp,
   extraHrs: 10,
 };
 
 let mgr = {
+  empId: "32",
+  empName: "Mark LaRue",
   baseSal: 6000,
   __proto__: emp,
   extraHrs: 15,
 };
 
 let srMgr = {
+  empId: "18",
+  empName: "Fran Green",
   baseSal: 8000,
   __proto__: emp,
   extraHrs: 12,
@@ -27,8 +33,18 @@ let srMgr = {
 function calcSal(emp) {
   let totSal = emp.baseSal + emp.extraHrs * 50;
   console.log(totSal);
-}
+};
 
 calcSal(consultant);
-calcSal(mgr);
-calcSal(srMgr);
+let consultantSal = calcSal(consultant);
+let mgrSal = calcSal(mgr);
+let srMgrSal = calcSal(srMgr);
+
+console.log(consultant.empName + " has made $" + consultantSal + " and has worked " + consultant.extraHrs + " extra hours.");
+console.log(mgr.empName + " has made $" + mgrSal + " and has worked " + mgr.extraHrs + " extra hours.");
+console.log(srMgr.empName + " has made $" + srMgrSal + " and has worked " + srMgr.extraHrs + " extra hours.");
+
+// calcSal(mgr);
+// calcSal(srMgr);
+
+
